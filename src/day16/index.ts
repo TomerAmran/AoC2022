19e73c4efedbe0244d638d074578568df2f91f94
tomera <tomera@wix.com>
2022-12-19 13:29:33 +0200

    good part 1 16

--- src/day16/index.ts
+++ src/day16/index.ts
@@ -1,7 +1,16 @@
 import run from "aocrunner";
 import lodash from 'lodash'
 const parseInput = (rawInput: string) => rawInput;
-type Node = { source: string, rate: number, targets: string[], opened: Set<string>, max: number, tick: number, prev?: string }
+type Node = {
+  source: string,
+  rate: number,
+  targets: string[],
+  opened: Set<string>,
+  max: number,
+  tick: number,
+  prev?: string,
+  distanceMap?: Record<string, number>
+}
 const copy = (_: Node): Node => {
   const x: Node = JSON.parse(JSON.stringify(_))
   x.opened = copySet(_.opened)
@@ -12,45 +21,69 @@ function copySet(set: Set<any>) {
 }
 const part1 = (rawInput: string) => {
   const regex = /Valve (.+) has flow rate=(\d+); tunnel(s?) lead(s?) to valve(s?) ((.{2}(,?)( ?))+)/
-  const input: Node[] = parseInput(rawInput).split('\n').map(_ => _.match(regex))
-    .map(([, source, rate, , , , ...targets]) => ({ source, rate: +rate, targets: targets.slice(0, -3)[0].split(', '), opened: new Set(), max: 0, tick: 30 }));
+  const input: any[] = parseInput(rawInput).split('\n').map(_ => _.match(regex))
+    .map(([, source, rate, , , , ...targets]) =>
+    ({
+      source, rate: +rate, targets: targets.slice(0, -3)[0].split(', ')
+      , opened: new Set(), max: 0, tick: 30, distanceMap: {}
+    }));
   const nodes: Record<string, Node> = input.reduce((prev, curr) => {
     return {
       ...prev,
       [curr.source]: curr
     }
   }, {})
-  let max = 0
-  let runs = 0
-  function rec(curr: Node) {
-    // console.log(runs++);
-    console.log(max);
+  const nonZeroValves = input.filter(_ => _.rate > 0).concat(nodes['AA'])
 
-    // console.log(curr.tick, curr.source);
-    max = Math.max(max, curr.max)
-    if (curr.tick === 0) { return }
-    if (!curr.opened.has(curr.source)) {
-      const newMax = curr.max + (curr.rate * (curr.tick - 1))
-      const next = copy(curr)
-      next.max = newMax
-      next.opened.add(curr.source)
-      next.prev = curr.source
-      next.tick = curr.tick - 1
-      rec(next)
-    }
-    if (curr.opened.size === input.length) { return }
-    curr.targets.forEach(target => {
-      const next = copy(nodes[target])
-      next.opened = copySet(curr.opened)
-      next.max = curr.max
-      next.prev = curr.source
-      next.tick = curr.tick - 1
-      rec(next)
+  nonZeroValves.forEach(valve1 => {
+    nonZeroValves.filter(_ => _ !== valve1).forEach(valve2 => {
+      const q: [Node, number][] = [[valve1, 0]]
+      const visited = new Set<string>()
+      while (q.length > 0) {
+        const [node, distance] = q.pop()
+        visited.add(node.source)
+        if (node === valve2) {
+          valve1.distanceMap[valve2.source] = distance
+          break
+        }
+        else {
+          node.targets.filter(_ => !visited.has(_)).forEach((target) => {
+            q.unshift([nodes[target], distance + 1])
+          })
+        }
+      }
     })
+  })
+  // console.log(nonZeroValves.map(_ => ([_.source, _.distanceMap])));
 
+  const memo = {}
+
+  function rec(node: string, opened: Set<string>, tick: number) {
+    if (tick === 0) return 0;
+    const pathOptionsResults = Object.entries(nodes[node].distanceMap)
+      .filter(([id, distance]) => !opened.has(id) && tick > distance)
+      .map(([targetNode, distance]) => {
+        const remaining = tick - distance - 1;
+        const pressure = nodes[targetNode].rate * remaining;
+        const newOpened = new Set(opened).add(targetNode)
+        const key = `${targetNode}_${remaining}_${[...newOpened.keys()].join('_')}`
+        // console.log(key)
+        if (memo[key]) {
+          console.log('hit')
+          return pressure + memo[key]
+        }
+        else {
+          // console.log('miss')
+          const nextRec = rec(targetNode, newOpened, remaining)
+          memo[key] = nextRec
+          return pressure + nextRec
+        }
+      })
+    return Math.max(0, ...pathOptionsResults);
   }
-  rec(nodes['AA'])
-  return max
+
+  return rec('AA', new Set(), 30)
+
 };
 
 const part2 = (rawInput: string) => {
@@ -80,10 +113,19 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+        Valve BB has flow rate=13; tunnels lead to valves CC, AA
+        Valve CC has flow rate=2; tunnels lead to valves DD, BB
+        Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+        Valve EE has flow rate=3; tunnels lead to valves FF, DD
+        Valve FF has flow rate=0; tunnels lead to valves EE, GG
+        Valve GG has flow rate=0; tunnels lead to valves FF, HH
+        Valve HH has flow rate=22; tunnel leads to valve GG
+        Valve II has flow rate=0; tunnels lead to valves AA, JJ
+        Valve JJ has flow rate=21; tunnel leads to valve II`,
+        expected: 1707,
+      },
     ],
     solution: part2,
   },
